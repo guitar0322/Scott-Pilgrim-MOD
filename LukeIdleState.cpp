b35af2eb731a279ced03b37a4938ccd3c6ee6b9d
Lee Si Young <66060749+Shung-2@users.noreply.github.com>
2021-06-29 10:07:48 +0900

    적 이미지 추가
    jesse를 제외한 적 이미지 추가

--- LukeIdleState.cpp
+++ LukeIdleState.cpp
@@ -20,9 +20,9 @@ void LukeIdleState::Enter(EnemyAI* enemy)
 {
 	_idelTime = 0;
 	if (enemy->enemyinfo->GetDir() == false)
-		enemy->ChangeClip("luke_idle_right", true);
+		enemy->ChangeClip("lee_idle_right", true);
 	else
-		enemy->ChangeClip("luke_idle_left", true);
+		enemy->ChangeClip("lee_idle_left", true);
 }
 
 void LukeIdleState::Exit(EnemyAI* enemy)
